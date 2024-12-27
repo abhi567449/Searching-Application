eaef690d4718dede130278b5c482de87ce1e4cbe
abhi67449 <abhi67449@gmail.com>
2024-12-27 11:33:35 +0530

    Dom

---
+++ Assgnment.js
@@ -0,0 +1,10 @@
+
+var divTag = document.getElementById("myDiv")
+
+divTag.style.color="blue";
+divTag.style.backgroundColor= "yellow";
+divTag.style.width="300px"
+divTag.style.height="300px"
+divTag.style.padding="20px"
+divTag.style.margin="20px"
+divTag.style.fontWeight="bold"
